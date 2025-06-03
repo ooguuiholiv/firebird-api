5781f4a30c286b35bc244ddb503a2b5d853249cc
Guilherme Franco <francoguilherme.sec@gmail.com>
2025-06-03 11:01:27 -0300

    :sparkles:  feat:  alter route /v¹-²/centro-custo

--- index.js
+++ index.js
@@ -226,62 +226,60 @@ LEFT JOIN fccusto ON fccusto.cod = p.ccusto_tratado;
 app.get("/v2/centro-custo", async (req, res) => {
   const sql = `
     WITH Preferido_CentroCusto AS (
-    SELECT
+      SELECT
         flan.idlan,
-        CAST(COALESCE(frc.ccusto, fe.ccusto, flan.ccusto) AS VARCHAR(100) CHARACTER SET WIN1252) AS ccusto_tratado,
+        COALESCE(frc.ccusto, fe.ccusto, flan.ccusto) AS ccusto_tratado,
         frc.ccusto AS rateio_de_cc,
         CASE 
-            WHEN flan.historico CONTAINING 'BAIXA PARCIAL' THEN flan.valorbaixado
-            WHEN flan.historico CONTAINING 'IMP_RET' THEN flan.valorbaixado
-            ELSE frc.valor
+          WHEN COALESCE(flan.historico, '') CONTAINING 'BAIXA PARCIAL' THEN flan.valorbaixado
+          WHEN COALESCE(flan.historico, '') CONTAINING 'IMP_RET' THEN flan.valorbaixado
+          ELSE frc.valor
         END AS valor_cc,
         flan.ccusto AS cc,
         fe.ccusto AS cc_extrato
-    FROM flan
-    LEFT JOIN (
+      FROM flan
+      LEFT JOIN (
         SELECT idlan, ccusto, valor
         FROM flanratccusto
         WHERE origem = 'L'
-    ) frc ON frc.idlan = flan.idlan
-    LEFT JOIN (
+      ) frc ON frc.idlan = flan.idlan
+      LEFT JOIN (
         SELECT numerodocumento, cnpjcpf, ccusto
         FROM fextrato
-    ) fe ON fe.numerodocumento = 
-                CASE 
-                    WHEN flan.statuslan = 'B' THEN flan.numerodocumento || '/' || COALESCE(CAST(flan.parcela AS VARCHAR(10)), '')
-                    ELSE flan.numerodocumento
-                END
-            AND fe.cnpjcpf = flan.codcfo
-)
-SELECT 
-    flan.datavencimento, 
-    flan.databaixa,
-    CASE 
-        WHEN flan.statuslan = 'B' THEN flan.numerodocumento || '/' || COALESCE(CAST(flan.parcela AS VARCHAR(10)), '')
-        ELSE flan.numerodocumento
-    END AS numerodocumento, 
-    flan.parcela,
-    fcfo.nomefantasia,
-    fcfo.cgccfo AS CNPJ_CPF, 
-    flan.valororiginal,
-    flan.valorbaixado,
-    p.rateio_de_cc AS Rateio_de_CC,
-    p.valor_cc AS VALOR_CC,
-    p.cc AS CC,
-    p.cc_extrato AS CC_extrato,
-    p.ccusto_tratado AS CC_tratado,
-    CASE 
+      ) fe ON fe.numerodocumento = 
+        CASE 
+          WHEN flan.statuslan = 'B' THEN COALESCE(flan.numerodocumento, '') || '/' || COALESCE(CAST(flan.parcela AS VARCHAR(10)), '')
+          ELSE COALESCE(flan.numerodocumento, '')
+        END
+        AND fe.cnpjcpf = flan.codcfo
+    )
+    SELECT 
+      flan.datavencimento, 
+      flan.databaixa,
+      CASE 
+        WHEN flan.statuslan = 'B' THEN COALESCE(flan.numerodocumento, '') || '/' || COALESCE(CAST(flan.parcela AS VARCHAR(10)), '')
+        ELSE COALESCE(flan.numerodocumento, '')
+      END AS numerodocumento, 
+      flan.parcela,
+      fcfo.nomefantasia,
+      fcfo.cgccfo AS CNPJ_CPF, 
+      flan.valororiginal,
+      flan.valorbaixado,
+      p.rateio_de_cc AS Rateio_de_CC,
+      p.valor_cc AS VALOR_CC,
+      p.cc AS CC,
+      p.cc_extrato AS CC_extrato,
+      p.ccusto_tratado AS CC_tratado,
+      CASE 
         WHEN flan.pagrec = 'P' THEN 'PAGAR'
         WHEN flan.pagrec = 'R' THEN 'RECEBER'
         ELSE 'DESCONHECIDO' 
-    END AS Tipo_Transacao,
-    --fccusto.descricao AS Descricao_Centro_Custo,
-    flan.statuslan AS Status_do_lancamento
-  --flan.historico
-FROM flan
-LEFT JOIN fcfo ON fcfo.codcfo = flan.codcfo
-LEFT JOIN Preferido_CentroCusto p ON p.idlan = flan.idlan
-LEFT JOIN fccusto ON fccusto.cod = p.ccusto_tratado;
+      END AS Tipo_Transacao,
+      flan.statuslan AS Status_do_lancamento
+    FROM flan
+    LEFT JOIN fcfo ON fcfo.codcfo = flan.codcfo
+    LEFT JOIN Preferido_CentroCusto p ON p.idlan = flan.idlan
+    LEFT JOIN fccusto ON fccusto.cod = p.ccusto_tratado;
   `;
 
   console.log(`Rota /v2/centro-custo acessada. Origin: ${req.get("origin")}`);
